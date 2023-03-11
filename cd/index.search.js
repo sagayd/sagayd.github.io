678dda05e726a2163c4b2b30b3c2bccbb121b822
sagayd <sagayaraj.d@opsmx.io>
2023-03-11 19:27:35 +0000

    Update site pages by GH actions

---
+++ cd/index.search.js
@@ -0,0 +1,12 @@
+var relearn_search_index=[{content:` Lets explore ArgoCD
+`,description:"",tags:null,title:"ArgoCD",uri:"/argocd/index.html"},{content:` Lets explore Blog posts
+`,description:"",tags:null,title:"Blogs",uri:"/blogs/index.html"},{content:` Lets explore Technical Docs created by Sagayaraj
+`,description:"",tags:null,title:"Site Home",uri:"/index.html"},{content:` Lets explore Spinnaker documentation site
+`,description:"",tags:null,title:"Spinnaker",uri:"/spinnaker/index.html"},{content:` Lets check how to deploy Spinnaker
+Requirements Kubernetes Cluster
+Kubectl CLI
+More to come `,description:"",tags:null,title:"How to Deploy Spinnaker",uri:"/blogs/how-to-deploy-spinnaker/index.html"},{content:` Lets check how to deploy Spinnaker
+Requirements Kubernetes Cluster
+Kubectl CLI
+More to come `,description:"",tags:null,title:"How to Deploy Spinnaker",uri:"/posts/how-to-deploy-spinnaker/index.html"},{content:"",description:"",tags:null,title:"Posts",uri:"/posts/index.html"},{content:` Hello world!
+`,description:"",tags:null,title:"My first post",uri:"/posts/first/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"}]
